ed6dba191fc5df4f2952e7a0c189b7344cc5fa06
Uday Satapathy <uday.satapathy@databricks.com>
2022-09-14 10:43:02 -0400

    renamed vars and added comments

--- example-mtj/mtj.tf
+++ example-mtj/mtj.tf
@@ -7,7 +7,7 @@ data "databricks_node_type" "smallest" {
   local_disk = true
 }
 
-
+// provisioning notebooks
 resource "databricks_notebook" "nb_1" {
   source = "/Users/uday.satapathy/Documents/src/GitHub/test-repo/nb-1.py"
   path   = "${data.databricks_current_user.me.home}/Terraform/notebooks/nb-1"
@@ -23,8 +23,9 @@ resource "databricks_notebook" "nb_3" {
   path   = "${data.databricks_current_user.me.home}/Terraform/notebooks/nb-3"
 }
 
+// create multi-task job cluster
 resource "databricks_cluster" "shared_01" {
-  cluster_name            = "uday terraform mtj"
+  cluster_name            = "uday_terraform_multi-task-job_cluster_01"
   spark_version           = data.databricks_spark_version.latest_lts.id
   node_type_id            = data.databricks_node_type.smallest.id
   autotermination_minutes = 20
@@ -34,11 +35,12 @@ resource "databricks_cluster" "shared_01" {
   }
 }
 
+// create a multi-task job
 resource "databricks_job" "this" {
   name = "Job with multiple tasks"
 
   job_cluster {
-    job_cluster_key = "j"
+    job_cluster_key = "uday_terraform_multi-task-job_cluster_02"
     new_cluster {
       num_workers   = 2
       spark_version = data.databricks_spark_version.latest.id
@@ -47,7 +49,7 @@ resource "databricks_job" "this" {
   }
 
   task {
-    task_key = "a"
+    task_key = "task-a"
 
     new_cluster {
       num_workers   = 1
@@ -61,10 +63,10 @@ resource "databricks_job" "this" {
   }
 
   task {
-    task_key = "b"
+    task_key = "task-b"
     //this task will only run after task a
     depends_on {
-      task_key = "a"
+      task_key = "task-a"
     }
 
     notebook_task {
@@ -76,9 +78,9 @@ resource "databricks_job" "this" {
   }
 
   task {
-    task_key = "c"
+    task_key = "task-c"
 
-    job_cluster_key = "j"
+    job_cluster_key = "uday_terraform_multi-task-job_cluster_02"
 
     notebook_task {
       notebook_path = "${databricks_notebook.nb_3.path}"
@@ -86,12 +88,12 @@ resource "databricks_job" "this" {
   }
   
   task {
-    task_key = "d"
+    task_key = "task-d"
     depends_on {
-        task_key = "c"
+        task_key = "task-c"
     }
     depends_on {
-        task_key = "a"
+        task_key = "task-a"
     }
     existing_cluster_id = "${databricks_cluster.shared_01.id}"
 
